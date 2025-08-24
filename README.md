6598387e65b6fe41e03437355673e4d7c0eccb9e
Manik2375 <manikkpr2375@gmail.com>
2025-08-24 14:11:35 +0530

    INITIAL setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# receipt-manager-app
-A react native application to manage your payment receipts or maybe warranty receipts
---
+++ src/lib/appwrite.ts
@@ -0,0 +1,11 @@
+import { Client, Databases, Account } from "react-native-appwrite";
+
+export const client = new Client();
+client
+  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT ?? "")
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID ?? "") // Replace with your project ID
+  .setPlatform('com.receipt.manager.com');
+
+
+export const account = new Account(client);
+export const databases = new Databases(client);
